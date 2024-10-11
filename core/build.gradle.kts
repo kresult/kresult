2f7f61497d9d4ecf07e34050338f03a987004a35
frne-trifork <frne@trifork.com>
2024-10-11 11:31:16 +0200

    Remove (so far) unused android build target

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     //alias(libs.plugins.androidLibrary)
@@ -9,16 +6,6 @@ plugins {
 
 kotlin {
     jvm()
-    //androidTarget {
-    //    publishLibraryVariants("release")
-    //    @OptIn(ExperimentalKotlinGradlePluginApi::class)
-    //    compilerOptions {
-    //        jvmTarget.set(JvmTarget.JVM_1_8)
-    //    }
-    //}
-    //iosX64()
-    //iosArm64()
-    //iosSimulatorArm64()
     linuxX64()
 
     sourceSets {
@@ -34,11 +21,3 @@ kotlin {
         }
     }
 }
-
-// android {
-//     namespace = "org.jetbrains.kotlinx.multiplatform.library.template"
-//     compileSdk = libs.versions.android.compileSdk.get().toInt()
-//     defaultConfig {
-//         minSdk = libs.versions.android.minSdk.get().toInt()
-//     }
-// }
