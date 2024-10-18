12f9d7b941a22811582d26dbe1fc11ba900118fc
frne <fneff89@gmail.com>
2024-10-18 14:31:14 +0200

    Added transformation from KResult to Result

--- libs/kresult-core/src/commonMain/kotlin/io/kresult/core/GenericTypeExtensions.kt
+++ libs/kresult-core/src/commonMain/kotlin/io/kresult/core/GenericTypeExtensions.kt
@@ -11,6 +11,20 @@ fun <T> Result<T>.asKResult(): KResult<Throwable, T> =
     { e -> Failure(e) },
   )
 
+/**
+ * Transforms a [KResult] to a Kotlin native [kotlin.Result]
+ *
+ * As Kotlin's [Result] strinctly expects a [Throwable] as error / failure type, this method is only available on
+ * [KResult] instances having a [Throwable] subtype on their [Failure] side.
+ *
+ * @since 0.2.0
+ * @see KResult.asKResult to transform a Kotlin [Result] to a [KResult]
+ */
+fun <E : Throwable, T> KResult<E, T>.asResult(): Result<T> =
+  fold(
+    { t -> Result.failure(t) },
+    { e -> Result.success(e) }
+  )
 fun <A> A.asFailure(): KResult<A, Nothing> =
   Failure(this)
 
