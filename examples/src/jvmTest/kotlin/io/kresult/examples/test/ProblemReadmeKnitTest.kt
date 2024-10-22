ba6d5af3e73cd4c6001d8e733efbb694304c1dfb
frne <fneff89@gmail.com>
2024-10-22 17:58:12 +0200

    Added kresult-problem module readme

---
+++ examples/src/jvmTest/kotlin/io/kresult/examples/test/ProblemReadmeKnitTest.kt
@@ -0,0 +1,12 @@
+// This file was automatically generated from README.md by Knit tool. Do not edit.
+package io.kresult.examples.examples.test
+
+import kotlin.test.Test
+import kotlinx.coroutines.test.runTest
+
+class ProblemReadmeKnitTest {
+  @Test fun exampleProblemreadme01() = runTest {
+    io.kresult.examples.exampleProblemreadme01.test()
+  }
+
+}
